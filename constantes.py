9756f03d739b8b25f1e6f1361da1423483eb89df
Gabriel Henrique Silva <gabrielhs2@al.insper.edu.br>
2025-04-08 14:46:09 -0300

    Constantes

---
+++ constantes.py
@@ -0,0 +1,6 @@
+Cvidro = 0.16     #calor específico do vidro
+Ccoca = 0.9      #calor específico da coca
+ktampa = 0.2     #condutibilidade do vidro 
+kvidro = 1       #condutibilidade da tampa
+h =  9.98         #coeficiente de transferencia convectiva do ar
+Tamb = 25        #temperatura ambiente
