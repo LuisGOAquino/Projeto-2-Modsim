5eee01a7f23249dc464b293fd5b1590d32c693d0
Gabriel Henrique Silva <gabrielhs2@al.insper.edu.br>
2025-04-09 14:16:32 -0300

    medir tempo que bate 24.5°C

--- funcoes.py
+++ funcoes.py
@@ -10,7 +10,7 @@ def Q1(T):
     RcondVf = e / ((2*kvidro)*2*pi*(r + 0.75*e)*altura)
     RconvV = 1 / (h*2*pi*(r+e)*altura)
     F1 = (Tamb - Tv)/(RcondVf + RconvV)
-    return abs(F1)
+    return F1
 
 def Q2(T):
     Tv=T[0]
@@ -43,11 +43,12 @@ def modelo(T, t):
     dTvdt = (Qum-Qdois)/(Mvi*Cvidro)
     dxdt = [dTrdt, dTvdt]
     return dxdt
+
 x=273
 y=273
 T_0 = [x,y]
 dt = 0.01
-tempo = np.arange(0,70000,dt)
+tempo = np.arange(0,36000,dt)
 
 
 a = odeint(modelo, T_0, tempo)
@@ -55,6 +56,11 @@ a = odeint(modelo, T_0, tempo)
 Tv = a[:,0]
 Tr = a[:,1]
 
+for i in range(len(Tv)):
+    if Tv[i]>=297.5:
+        itemp = i*dt
+        break
+print(itemp)        
 
 plt.plot(tempo, Tv, label="Temperatura do vidro")
 plt.plot(tempo, Tr,'r--' , label="Temperatura do refrigerante")
